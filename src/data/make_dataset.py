bc6a08b13eb5a9e1b82dc4e229a9227cee451e9a
ixa4823 <igor.adamiec@iff.com>
2023-10-30 16:15:37 +0100

    TAXI-06 splitter as a script, added to make_dataset.py

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,1 +1,10 @@
-print("Making dataset")
+from data_split import train_test_split
+
+
+if __name__ == "__main__":
+    print("Making dataset")
+    print("Splitting data into train and test sets")
+    # TODO: arguments reading from config file
+    train_test_split(
+        input_file="final_taxi_data.parquet", test_size=0.3, save_files=True, output_files=("train_data", "test_data")
+    )
