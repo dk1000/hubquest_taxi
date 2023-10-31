370876ffb2fac221064ea9eab5a827ff9abfe470
AdamJedz <adamiec.igor@gmail.com>
2023-10-31 11:04:04 +0100

    TAXI-06 train test split (#7)
    * TAXI-06 train-test split
    
    * TAXI-06 train-test split done by the date
    
    * TAXI-06 splitting data as a script
    
    * TAXI-06 splitter as a script, added to make_dataset.py
    
    ---------
    
    Co-authored-by: ixa4823 <igor.adamiec@iff.com>

---
+++ src/data/data_split.py
@@ -0,0 +1,20 @@
+import pandas as pd
+from pathlib import Path
+
+
+def train_test_split(
+    input_file: str = "final_taxi_data.parquet",
+    test_size: float = 0.3,
+    save_files: bool = True,
+    output_files: tuple = ("train_data", "test_data"),
+):
+    data_path = Path(__file__).parent.parent.parent / "data" / "raw"
+    print(data_path)
+    df = pd.read_parquet(data_path / input_file).sort_values("pickup_datetime").reset_index(drop=True)
+    split_idx = int(round((1 - test_size) * len(df), 0))
+    train_set, test_set = df.iloc[:split_idx], df.iloc[split_idx:]
+    if save_files:
+        train_set.to_parquet(data_path / (output_files[0] + ".parquet"))
+        test_set.to_parquet(data_path / (output_files[1] + ".parquet"))
+        print(f"Train and test set saved into directory: {str(data_path)}")
+    return train_set, test_set
