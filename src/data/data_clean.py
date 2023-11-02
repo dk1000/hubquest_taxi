ef603d73e09369e2477a1b64d0283ce9269bee0e
ixa4823 <igor.adamiec@iff.com>
2023-11-02 13:29:09 +0100

    TAXI-08 trip_distance col added

--- src/data/data_clean.py
+++ src/data/data_clean.py
@@ -10,8 +10,9 @@ class DataCleaner(BaseEstimator, TransformerMixin):
     Description: Class that removes bad/missing/outlier data from the training set
     """
 
-    def __init__(self, cols_to_check_na: list = None):
+    def __init__(self, cols_to_check_na: list = None, iqr_par: float = 1.5):
         self.cols_to_check_na = cols_to_check_na
+        self.iqr_par = iqr_par
         pass
 
     def fit(self, X: pd.DataFrame, y: pd.Series = None):
@@ -20,21 +21,29 @@ class DataCleaner(BaseEstimator, TransformerMixin):
     def transform(self, X: pd.DataFrame, y: pd.Series = None):
         print("Cleaning the data")
         print(f"Input data: {len(X)} observations")
-        cols_to_iqr = ["fare_amount"]
+        cols_to_iqr = ["fare_amount", "trip_distance"]
         iqr_dict = {col: np.nanquantile(X[col], q=[0.25, 0.75]) for col in cols_to_iqr}
 
         X = X.loc[
             (X["passenger_count"] > 0)
-            & (X["pickup_longitude"].between(-76, 71, inclusive="both"))
+            & (X["pickup_longitude"].between(-76, -71, inclusive="both"))
             & (X["pickup_latitude"].between(39, 42, inclusive="both"))
-            & (X["dropoff_longitude"].between(-76, 71, inclusive="both"))
+            & (X["dropoff_longitude"].between(-76, -71, inclusive="both"))
             & (X["dropoff_latitude"].between(39, 42, inclusive="both"))
             & (X["ratecodeid"].isin([1, 2, 3, 4, 6]))
             & (
                 X["fare_amount"].between(
                     0,
                     iqr_dict.get("fare_amount")[1]
-                    + 1.5 * (iqr_dict.get("fare_amount")[1] - iqr_dict.get("fare_amount")[0]),
+                    + self.iqr_par * (iqr_dict.get("fare_amount")[1] - iqr_dict.get("fare_amount")[0]),
+                    inclusive="right",
+                )
+            )
+            & (
+                X["trip_distance"].between(
+                    0,
+                    iqr_dict.get("trip_distance")[1]
+                    + self.iqr_par * (iqr_dict.get("trip_distance")[1] - iqr_dict.get("trip_distance")[0]),
                     inclusive="right",
                 )
             )
