5e1f5baac101f8d0f8fca05d90f45aa21f88b955
ixa4823 <igor.adamiec@iff.com>
2023-11-06 09:45:59 +0100

    TAXI-08 resolved conflicts

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -1,12 +1,12 @@
 import logging
+from pathlib import Path
 
 import pandas as pd
-
-from data_split import train_test_split
 from data_clean import DataCleaner
-
+from data_split import train_test_split
 from sklearn.pipeline import Pipeline
-from pathlib import Path
+
+logging.basicConfig(level=logging.INFO)
 
 # TODO: cleaner_cols_to_check_na should be read from config.yml - those should be columns used by preprocessing/model
 preparation_pipeline = Pipeline([("cleaner", DataCleaner())]).set_params(cleaner__cols_to_check_na=None)
@@ -16,7 +16,6 @@ def save_processed_training_data(data: pd.DataFrame, out_file_name: str):
     data_path = Path(__file__).parent.parent.parent / "data" / "interim" / (out_file_name + ".parquet")
     data.to_parquet(data_path)
 
-logging.basicConfig(level=logging.INFO)
 
 if __name__ == "__main__":
     print("Making dataset")
@@ -24,8 +23,6 @@ if __name__ == "__main__":
     logging.info("Splitting data into train and test sets")
     # TODO: arguments reading from config file
     train_data, test_data = train_test_split(
-        input_file="final_taxi_data.parquet", test_size=0.3, save_files=True, output_files=("train_data", "test_data")
-    train_test_split(
         input_file="final_taxi_data.parquet",
         test_size=0.3,
         save_files=True,
