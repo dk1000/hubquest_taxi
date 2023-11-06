48c00d1dec50e72c469920786b267b3d263a4e40
AdamJedz <adamiec.igor@gmail.com>
2023-11-06 10:15:38 +0100

    TAXI-08 Data cleaner (#11)
    * TAXI-08 Data cleaner
    
    * TAXI-08 Added Na handling
    
    * TAXI-08 trip_distance col added
    
    * TAXI-08 resolved conflicts
    
    ---------
    
    Co-authored-by: ixa4823 <igor.adamiec@iff.com>

--- src/data/data_split.py
+++ src/data/data_split.py
@@ -9,13 +9,18 @@ def train_test_split(
     save_files: bool = True,
     output_files: tuple = ("train_data", "test_data"),
 ):
+    print("Splitting data into train and test sets")
+
     data_path = Path(__file__).parent.parent.parent / "data" / "raw"
     print(data_path)
     df = pd.read_parquet(data_path / input_file).sort_values("pickup_datetime").reset_index(drop=True)
+    print(f"Input data: {len(df)} observations")
     split_idx = int(round((1 - test_size) * len(df), 0))
     train_set, test_set = df.iloc[:split_idx], df.iloc[split_idx:]
+    print(f"Output data:\n\ttraining set: {len(train_set)} observations\n\ttest set: {len(test_set)} observations")
     if save_files:
         train_set.to_parquet(data_path / (output_files[0] + ".parquet"))
         test_set.to_parquet(data_path / (output_files[1] + ".parquet"))
         print(f"Train and test set saved into directory: {str(data_path)}")
+
     return train_set, test_set
