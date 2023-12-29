6c3b1b7886c06c839f0bd483f6c0c3253dbf5452
Igor Adamiec <adamiec.igor@gmail.com>
2023-12-29 10:44:09 +0100

    TAXI-21 Saving pipeline (#19)
    * TAXI-21 added columns to drop in the beginning of build_features.py (as well as filling NAs with 0)
    Added saving pipeline to the file so it can be retrieved in prediction phase
    
    * TAXI-21 improved logging
    
    * TAXI-21 changed "cols to drop" to "cols to keep", introduced hashing in pipeline name
    
    * TAXI-21 changed pipeline directory
    
    ---------
    
    Co-authored-by: ixa4823 <igor.adamiec@iff.com>

--- src/features/build_features.py
+++ src/features/build_features.py
@@ -1,13 +1,16 @@
 import logging
+import hashlib
 
 import pandas as pd
 from sklearn.pipeline import Pipeline
+import joblib
+import os
 
 from src.features.cluster_features import ClusterLocationTransformer, ClusterTripTransformer
 from src.features.geo_features import GeoDataTransformer
 from src.features.time_features import DateTimeTransformer
 from src.features.trip_distance import ModelTripDistanceTransformer
-from utils import get_data_path
+from utils import get_model_path, get_data_path
 
 logging.basicConfig(level=logging.INFO)
 
@@ -23,6 +26,8 @@ class FeaturesBuilder:
         input_test_data: str,
         save_data: bool,
         output_file_names: list,
+        pipeline_dir: str,
+        cols_to_keep: list = None,
     ):
         self.feature_pipeline = Pipeline(
             [
@@ -52,12 +57,19 @@ class FeaturesBuilder:
         self.input_test_data = input_test_data
         self.save_data = save_data
         self.output_file_names = output_file_names
+        self.cols_to_keep = cols_to_keep
+        self.pipeline_dir = pipeline_dir
+        self.pipeline_path = get_model_path() / pipeline_dir
+
+        params = str(geodata) + str(clusters_location) + str(clusters_trip) + str(trip_distance_model)
+        params_md5 = hashlib.md5(params.encode("utf-8")).hexdigest()
+        self.pipeline_name = f"{params_md5}.pkl"
 
     def load_data(self):
         logging.info("Loading input data from files")
         path = get_data_path()
-        train_data = pd.read_parquet(path.joinpath(self.input_train_data + ".parquet"))
-        test_data = pd.read_parquet(path.joinpath(self.input_test_data + ".parquet"))
+        train_data = pd.read_parquet(path.joinpath(self.input_train_data + ".parquet"))[self.cols_to_keep].fillna(0)
+        test_data = pd.read_parquet(path.joinpath(self.input_test_data + ".parquet"))[self.cols_to_keep].fillna(0)
         return train_data, test_data
 
     def save_processed_data(self, train_data: pd.DataFrame, test_data: pd.DataFrame):
@@ -77,13 +89,21 @@ class FeaturesBuilder:
         test_data_prep = self.feature_pipeline.transform(test_data)
         return test_data_prep
 
+    def save_pipeline(self):
+        logging.info("Saving pipeline to the file")
+        if not os.path.exists(self.pipeline_path):
+            os.makedirs(self.pipeline_path)
+        joblib.dump(self.feature_pipeline, self.pipeline_path / self.pipeline_name)
+        logging.info(f"Pipeline saved as {self.pipeline_path / self.pipeline_name}")
+
     def build_features(self, train_data, test_data):
         logging.info("Start - Building Features")
         logging.info("Step 1. Running train pipeline")
-        train_data_prep = self.run_train_pipeline(train_data)
+        train_data_prep = self.run_train_pipeline(train_data[self.cols_to_keep].fillna(0))
         logging.info("Step 2. Running test pipeline")
-        test_data_prep = self.run_test_pipeline(test_data)
+        test_data_prep = self.run_test_pipeline(test_data[self.cols_to_keep].fillna(0))
         if self.save_data:
             self.save_processed_data(train_data_prep, test_data_prep)
+            self.save_pipeline()
         logging.info("End - building Features")
         return train_data_prep, test_data_prep
